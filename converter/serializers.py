a2542dfa37e0c1936b31ba8847e323475975c459
Frederico Leão <fredericoleaob@gmail.com>
2024-03-04 03:01:35 -0300

    fix amount max_digits

--- converter/serializers.py
+++ converter/serializers.py
@@ -4,8 +4,8 @@ class CurrencyConvertSerializer(serializers.Serializer):
     src_currency = serializers.CharField(max_length=5)
     tgt_currency = serializers.CharField(max_length=5)
     amount = serializers.DecimalField(
-        max_digits=12,
-        decimal_places=6,
+        max_digits=16,
+        decimal_places=7,
         error_messages={
             'invalid': 'O número informado é inválido',
             'max_digits': 'O número informado é inválido'})
